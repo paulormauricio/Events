bf0bb684dcba37cdb1552b8e61daf9a0d32ebf29
paulormauricio <paulormauricio@gmail.com>
2015-07-30 12:59:14 +0100

    Revert "Version 0.69"
    This reverts commit f1c501b4306b61c994de616d391327b935c471d6.

--- www/js/app.js
+++ www/js/app.js
@@ -31,9 +31,9 @@ angular.module('events',
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
 
-    // document.addEventListener("pause", function() {
-    //     console.log("The application when to background mode");
-    // }, false);
+    document.addEventListener("pause", function() {
+        alert("The application when to background mode");
+    }, false);
 
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
